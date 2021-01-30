40ed34dc298f82e13d399a5a75895cdae76bf5ac
dimplejoon <76550158+dimplejoon@users.noreply.github.com>
2021-01-30 22:07:20 +0900

    Day 7

--- app.js
+++ app.js
@@ -3,22 +3,24 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import { localsMiddleware } from "./middlewares"; //알파벳순으로 정리해라, 커넥션 라우터들 사이에 미들웨어는 위치해야한다
+import routes from "./routes"; //라우터들에서 routes를 사용하기 때문에 routes가 먼저나와야한다
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
-import routes from "./routes";
 // 디폴트해주지 않았던 것을 import할 때는 함수를 {} 안에 넣고 from 뒤에는 주소를 적어준다.
 
 
 
 const app = express(); //1. index.js가 실행되고 그 다음에 app(application)이 route가 존재하는지 확인한다
 
+app.use(helmet());
 app.set("view engine", "pug");
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(helmet());
 app.use(morgan("dev"));
+app.use(localsMiddleware); //이게 여기에 있는 이유는 아래로 가면 global이나 user router에서 localsMiddleware에 접근을 못한다!
 // express는 양파처럼 여러 layer가 있는데, middleware가 그 layer인 것이다.
 
 
---
+++ middlewares.js
@@ -0,0 +1,8 @@
+import routes from "./routes";
+
+export const localsMiddleware = (req, res, next) => {
+
+    res.locals.siteName = "JoonTube";
+    res.locals.routes = routes;
+    next(); // main에서 미들웨어가 커넥션과 라우트들 사이에 위치해 있으니까!
+}; // localsmiddelwares를 선언해서(만들어서) 전역 범위에 변수를 추가할 수 있게 했다. 템플릿 여기저기에 넣어줄 수 있고 따로 수정해주지 않고 이 코드만 수정하면 다른 템플릿들에도 다 적용될 것

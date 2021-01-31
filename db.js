d1fd030f70199ed6c91094fcb28713fbb005a8ba
dimplejoon <76550158+dimplejoon@users.noreply.github.com>
2021-01-31 22:58:39 +0900

    Day 8

---
+++ db.js
@@ -0,0 +1,52 @@
+export const videos = [
+    {
+        id: 333333,
+        title: 'Video awesome',
+        description: 'This is something I love',
+        views: 24,
+        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        creator: {
+            id: 121212,
+            name: "Sung Joon LEE_Zeno",
+            email: "dimple@joon.com"
+        }
+    },
+    {
+        id: 555555,
+        title: 'Video Super',
+        description: 'This is something I love',
+        views: 24,
+        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        creator: {
+            id: 121212,
+            name: "Sung Joon LEE_Zeno",
+            email: "dimple@joon.com"
+        }
+    },
+    {
+        id: 666666,
+        title: 'Video Nice',
+        description: 'This is something I love',
+        views: 24,
+        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        creator: {
+            id: 121212,
+            name: "Sung Joon LEE_Zeno",
+            email: "dimple@joon.com"
+        }
+    },
+    {
+        id: 777777,
+        title: 'Video Perfect',
+        description: 'This is something I love',
+        views: 24,
+        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        creator: {
+            id: 121212,
+            name: "Sung Joon LEE_Zeno",
+            email: "dimple@joon.com"
+        }
+    }
+
+
+]
